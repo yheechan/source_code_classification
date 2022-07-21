6c609c33829e43be18a1be8d62e4dabb270995f0
Heechan Yang <heechy06@naver.com>
2022-07-21 22:35:48 +0900

    opt KS test for cnn

--- src/initializer.py
+++ src/initializer.py
@@ -1,4 +1,5 @@
 import cnn
+import rnn
 import torch.optim as optim
 
 def initilize_model(device=None,
@@ -12,37 +13,44 @@ def initilize_model(device=None,
                     dropout=0.5,
                     learning_rate=0.01,
                     optimizerName="Adadelta",
-                    mome=0.8):
+                    mome=0.8,
+                    modelType="CNN"):
     """Instantiate a CNN model and an optimizer."""
 
     assert (len(filter_sizes) == len(num_filters)), "filter_sizes and \
     num_filters need to be of the same length."
 
-    # Instantiate CNN model
-    cnn_model = cnn.CNN_NLP(pretrained_embedding=pretrained_embedding,
-                        freeze_embedding=freeze_embedding,
-                        vocab_size=vocab_size,
-                        embed_dim=embed_dim,
-                        filter_sizes=filter_sizes,
-                        num_filters=num_filters,
-                        num_classes=num_classes,
-                        dropout=0.5)
+    if modelType=="CNN":
+        # Instantiate CNN model
+        model = cnn.CNN_NLP(pretrained_embedding=pretrained_embedding,
+                            freeze_embedding=freeze_embedding,
+                            vocab_size=vocab_size,
+                            embed_dim=embed_dim,
+                            filter_sizes=filter_sizes,
+                            num_filters=num_filters,
+                            num_classes=num_classes,
+                            dropout=0.5)
+    
+    elif modelType=="RNN":
+        model = rnn.RNNClassifier(vocab_size,
+                                  embed_dim,
+                                  )
     
     # Send model to `device` (GPU/CPU)
-    cnn_model.to(device)
+    model.to(device)
 
     # Instantiate Adadelta optimizer
     if optimizerName == "Adadelta":
-        optimizer = optim.Adadelta(cnn_model.parameters(),
+        optimizer = optim.Adadelta(model.parameters(),
                                     lr=learning_rate,
                                     rho=0.95)
-    elif optimizerName == "SDG":
-        optimizer = optim.SDG(cnn_model.parameters(),
+    elif optimizerName == "SGD":
+        optimizer = optim.SGD(model.parameters(),
                                 lr=learning_rate,
                                 momentum=mome)
     elif optimizerName == "Adam":
-        optimizer = optim.Adam(cnn_model.parameters(),
+        optimizer = optim.Adam(model.parameters(),
                                 lr=learning_rate)
 
-    return cnn_model, optimizer
+    return model, optimizer
 
